dedd954d7a3b3f411eb46f01e9006a05aa81ffea
Christian Gmeiner <christian.gmeiner@gmail.com>
2015-12-20 22:20:54 +0100

    etnaviv: add TGSI_OPCODE_POW implementation
    Add an implementation for the POW TGSI operation.  This can be
    performed by the following sequence: ex2(y*lg2(x))
    
    	LOG tmp.xxxx, void, void, src0.x
    	MUL tmp.yyyy, scr1.y, tmp.xxxx
    	EXP dst.xxxx, void, void, tmp.yyyy
    
    This fixes glean +fragProg1 "POW test (exponentiation)".
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/etnaviv_compiler.c
+++ src/gallium/drivers/etnaviv/etnaviv_compiler.c
@@ -1162,6 +1162,41 @@ static void etna_compile_pass_generate_code(struct etna_compile_data *cd, const 
                         .src[2] = src[0],
                         });
                 break;
+            case TGSI_OPCODE_POW: {
+                struct etna_native_reg temp = etna_compile_get_inner_temp(cd);
+
+                emit_inst(cd, &(struct etna_inst) {
+                        .opcode = INST_OPCODE_LOG,
+                        .sat = sat,
+                        .dst.use = 1,
+                        .dst.comps = INST_COMPS_X, /* temp.x */
+                        .dst.reg = temp.id,
+                        .src[2] = src[0],
+                        });
+
+                emit_inst(cd, &(struct etna_inst) {
+                        .opcode = INST_OPCODE_MUL,
+                        .sat = sat,
+                        .dst.use = 1,
+                        .dst.comps = INST_COMPS_Y, /* temp.y */
+                        .dst.reg = temp.id,
+                        .src[0] = src[1],
+                        .src[1].use = 1, /* temp.xxxx */
+                        .src[1].swiz = INST_SWIZ_BROADCAST(0),
+                        .src[1].rgroup = temp.rgroup,
+                        .src[1].reg = temp.id,
+                        });
+
+                emit_inst(cd, &(struct etna_inst) {
+                        .opcode = INST_OPCODE_EXP,
+                        .sat = sat,
+                        .dst = convert_dst(cd, &inst->Dst[0]),
+                        .src[2].use = 1, /* temp.yyyy */
+                        .src[2].swiz = INST_SWIZ_BROADCAST(1),
+                        .src[2].rgroup = temp.rgroup,
+                        .src[2].reg = temp.id,
+                        });
+                } break;
             case TGSI_OPCODE_XPD: {
                 /*
                  * MUL tTEMP.xyzw, src0.zxyw, src1.yzxw, void
@@ -1425,7 +1460,6 @@ INST_COMPS_W;
             case TGSI_OPCODE_PK2US:
             case TGSI_OPCODE_PK4B:
             case TGSI_OPCODE_PK4UB:
-            case TGSI_OPCODE_POW: /* lowered by mesa to ex2(y*lg2(x)) */
             case TGSI_OPCODE_ROUND:
             case TGSI_OPCODE_CLAMP:
             case TGSI_OPCODE_DP2A:
